7282fdaae8e5081617822d009a6397b437b544d5
M S Efendi <50564515+msfendi@users.noreply.github.com>
2024-10-25 16:05:01 +0700

    fix:delete constants IP local

--- lib/core/constants/variable.dart
+++ lib/core/constants/variable.dart
@@ -1,9 +1,3 @@
 class Variables {
-  static const String baseUrl = 'http://192.168.4.62:8000';
-  // static const String baseUrl = 'http://192.168.43.42:8000';
-  // static const String baseUrl = 'https://0923-202-152-139-242.ngrok-free.app';
-  // static const String rajaongkirUrl = 'https://pro.rajaongkir.com';
-  static const String rajaongkirUrl = 'https://api.rajaongkir.com';
-  // static const String rajaongkirApiKey = '605b5ffe8851b1b8ce0ef3639d2e673c';
-  static const String rajaongkirApiKey = '349c0335d5160eba1e318e40fe0aa8e8';
+
 }
